6fcd7492c81a7e3861a9412f026ede626199d559
stelvio7 <oreilly7@naver.com>
2017-05-20 18:29:03 +0900

    0520

---
+++ app/src/main/java/combodia/moatv5/model/Constant.java
@@ -0,0 +1,7 @@
+package combodia.moatv5.model;
+
+public class Constant {
+	public static String mainUrl = "http://box1.onnurytv.net";
+	public static String subUrl = "http://box1.onnurytv.net";
+	public static boolean isTest = false;
+}
---
+++ app/src/main/java/combodia/noh/util/EventProvider.java
@@ -0,0 +1,20 @@
+package combodia.noh.util;
+
+import com.squareup.otto.Bus;
+
+/**
+ * Created by HOME on 2017-04-10.
+ */
+
+public final class EventProvider {
+    private static final Bus BUS = new Bus();
+
+    public static Bus getInstance() {
+        return BUS;
+    }
+
+    private EventProvider() {
+        // No instances.
+    }
+
+}
